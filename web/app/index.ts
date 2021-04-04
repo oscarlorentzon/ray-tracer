d3c7c7718eeb6d50b79155ca631054fefbc9e045
Oscar Lorentzon <oscarlorentzon@users.noreply.github.com>
2021-04-04 11:07:40 +0200

    refactor: error helper method

--- web/app/index.ts
+++ web/app/index.ts
@@ -1,20 +1,24 @@
 import { Canvas } from "./Canvas.js";
 import { createCompatibility } from "./compatibility.js";
 
-(function main(): void {
-    const workerUrl = './web/worker/Renderer.worker.js';
-    const workerOptions: WorkerOptions = { type: 'module' };
-    const renderer = new Worker(workerUrl, workerOptions);
-    renderer.onerror = onError;
-
-    const canvas = new Canvas(renderer)
-    document.body.appendChild(canvas.canvas);
-    canvas.render();
-
-    function onError(event: ErrorEvent): void {
+function onError(canvas: Canvas)
+    : (event: ErrorEvent) => void {
+    return (event: ErrorEvent): void => {
         console.error(event.message);
         const compatibility = createCompatibility();
         document.body.removeChild(canvas.canvas);
         document.body.appendChild(compatibility);
     }
+}
+
+(function main(): void {
+    const workerUrl = './web/worker/Renderer.worker.js';
+    const workerOptions: WorkerOptions = { type: 'module' };
+    const renderer = new Worker(workerUrl, workerOptions);
+    const canvas = new Canvas(renderer)
+
+    renderer.onerror = onError(canvas);
+
+    document.body.appendChild(canvas.canvas);
+    canvas.render();
 })();
